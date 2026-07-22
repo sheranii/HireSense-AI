1d220c878cf6881da04faee19f2ed23b0ebef728
sheranii <194492330+sheranii@users.noreply.github.com>
2026-07-22 09:42:47 +0530

    chore: initialize HireSense AI backend with Spring Boot

---
+++ src/main/java/com/sherani/hiresense/HiresenseAiApplication.java
@@ -0,0 +1,13 @@
+package com.sherani.hiresense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiresenseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiresenseAiApplication.class, args);
+	}
+
+}
